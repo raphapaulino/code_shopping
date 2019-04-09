52e7b5973eb14b9fde8161b4e81bbc974f4a02ca
Raphael Paulino <raphael.paulino.web@gmail.com>
2019-04-09 19:04:06 -0300

    removed 'slug' field from fillable array

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -1,10 +1,27 @@
 <?php
+declare(strict_types=1); // type hinting
 
 namespace CodeShopping\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
 
 class Category extends Model
 {
-    protected $fillable = ['name', 'active', 'slug'];
+    use Sluggable;
+    protected $fillable = ['name', 'active'];
+
+    /**
+     * Return the sluggable configuration array for this model.
+     *
+     * @return array
+     */
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'name'
+            ]
+        ];
+    }
 }
