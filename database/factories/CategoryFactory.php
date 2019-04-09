8c8e288ff70381549de52da8c168eb793eb11577
Raphael Paulino <raphael.paulino.web@gmail.com>
2019-04-09 17:49:31 -0300

    updated category faker data

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -3,8 +3,12 @@
 use Faker\Generator as Faker;
 
 $factory->define(CodeShopping\Models\Category::class, function (Faker $faker) {
+
+    $words = $faker->words(3, true);
+
     return [
-        'name' => $faker->word,
+        'name' => $words,
+        'slug' => str_slug($words),
         'active' => $faker->boolean(50)
     ];
 });
