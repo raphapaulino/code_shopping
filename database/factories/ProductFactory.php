c7cc8d1420ee2fb1f6f620ea11adffbb567e6fb1
Raphael Paulino <raphael.paulino.web@gmail.com>
2019-04-10 12:56:51 -0300

    removed 'stock' from product faker

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -10,7 +10,7 @@ $factory->define(CodeShopping\Models\Product::class, function (Faker $faker) {
         'name' => $words,
         'description' => $faker->paragraph(4, true),
         'price' => $faker->randomFloat($nbMaxDecimals = 2, $min = 100, $max = 899),
-        'stock' => $faker->numberBetween($min = 1, $max = 100),
+        // 'stock' => $faker->numberBetween($min = 1, $max = 100),
         // 'slug' => str_slug($words),
         'active' => $faker->boolean(50)
     ];
