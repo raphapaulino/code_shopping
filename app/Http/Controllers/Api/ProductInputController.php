14e1dccf6b1fe49b0355f6ae01816202c7b98c2f
Raphael Paulino <raphael.paulino.web@gmail.com>
2019-04-13 02:06:47 -0300

    product input 'index' method implemented

--- app/Http/Controllers/Api/ProductInputController.php
+++ app/Http/Controllers/Api/ProductInputController.php
@@ -7,9 +7,16 @@ use Illuminate\Http\Request;
 use CodeShopping\Models\ProductInput;
 use CodeShopping\Models\Product;
 use CodeShopping\Http\Requests\ProductInputRequest;
+use CodeShopping\Http\Resources\ProductInputResource;
 
 class ProductInputController extends Controller
 {
+    public function index()
+    {
+        $inputs = ProductInput::with('product')->paginate();
+        return ProductInputResource::collection($inputs);
+    }
+
     public function store(ProductInputRequest $request, Product $product)
     {
         $productInput = new ProductInput;
