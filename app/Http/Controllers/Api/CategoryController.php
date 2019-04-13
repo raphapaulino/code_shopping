74e72c78327619bbd291bf411e3f77f024892ce9
Raphael Paulino <raphael.paulino.web@gmail.com>
2019-04-12 23:56:08 -0300

    created CategoryResource

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -6,6 +6,7 @@ use CodeShopping\Models\Category;
 use Illuminate\Http\Request;
 use CodeShopping\Http\Controllers\Controller;
 use CodeShopping\Http\Requests\CategoryRequest;
+use CodeShopping\Http\Resources\CategoryResource;
 
 class CategoryController extends Controller
 {
@@ -16,7 +17,7 @@ class CategoryController extends Controller
      */
     public function index()
     {
-        return Category::all();
+        return CategoryResource::collection(Category::all());
     }
 
     /**
@@ -29,7 +30,7 @@ class CategoryController extends Controller
     {
         $category = Category::create($request->all());
         $category->refresh();
-        return $category;
+        return new CategoryResource($category);
     }
 
     /**
@@ -40,7 +41,7 @@ class CategoryController extends Controller
      */
     public function show(Category $category)
     {
-        return $category;
+        return new CategoryResource($category);
     }
 
     /**
@@ -55,7 +56,7 @@ class CategoryController extends Controller
         $category->fill($request->all());
         $category->save();
 
-        return $category;
+        return new CategoryResource($category);
 
         // return response([], 204);
     }
