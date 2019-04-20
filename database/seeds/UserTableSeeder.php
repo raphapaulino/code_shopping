63d29abd41ef9025aae9825db76e08e2a0c9b7b4
Raphael Paulino <raphael.paulino.web@gmail.com>
2019-04-20 00:53:35 -0300

    UserTableSeeder created

---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use CodeShopping\Models\User;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(User::class, 1)
+            ->create([
+                'email' => 'admin@user.com'
+            ]);
+        factory(User::class, 50)
+            ->create();
+    }
+}
