73e382a2f158bac2c0dffb7a17a6b4a5ddb780db
Raphael Paulino <raphael.paulino.web@gmail.com>
2019-04-21 19:05:28 -0300

    increase faker numberBetween range

--- database/factories/ProductInputFactory.php
+++ database/factories/ProductInputFactory.php
@@ -4,6 +4,6 @@ use Faker\Generator as Faker;
 
 $factory->define(CodeShopping\Models\ProductInput::class, function (Faker $faker) {
     return [
-        'amount' => $faker->numberBetween(1, 20)
+        'amount' => $faker->numberBetween(1, 40)
     ];
 });
