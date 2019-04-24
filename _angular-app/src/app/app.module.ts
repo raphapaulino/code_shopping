913d81a154c9172ae7cb31e4d4b81d4738064b94
Raphael Paulino <raphael.paulino.web@gmail.com>
2019-04-23 23:04:41 -0300

    first ajax request to api initiated

--- _angular-app/src/app/app.module.ts
+++ _angular-app/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/pages/login/login.component';
-
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+// ES7 - JavaScript
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- _angular-app/src/app/components/pages/login/login.component.ts
+++ _angular-app/src/app/components/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'login',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  credentials = {
+    email: '',
+    password: ''
+  }
+
+  constructor(private http: HttpClient) { // injeção de dependencia automática
+
+  }
 
   ngOnInit() {
+
+  }
+
+  submit() {
+    alert('submeteu');
+    // envie uma requisição ajax com as credenciais para a API
+    this.http.post('http://localhost:8000/api/login', this.credentials)
+      .subscribe((data) => console.log(data));
+    return false; // evita que o form seja submetido
   }
 
 }
