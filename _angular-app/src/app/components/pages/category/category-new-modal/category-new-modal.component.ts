64f5dc22aa2d3d8de9d500c8c69c51734f27d86c
Raphael Paulino <raphael.paulino.web@gmail.com>
2019-08-17 22:59:14 -0300

    category new modal component created

---
+++ _angular-app/src/app/components/pages/category/category-new-modal/category-new-modal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-category-new-modal',
+  templateUrl: './category-new-modal.component.html',
+  styleUrls: ['./category-new-modal.component.css']
+})
+export class CategoryNewModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
